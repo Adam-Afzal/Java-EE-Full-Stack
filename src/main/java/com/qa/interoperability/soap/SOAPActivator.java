1c79f39e501fb3c52f0ff8a012d589f5fa5c7b0c
Adam-Afzal <adamafzal117@gmail.com>
2018-07-17 15:25:13 +0100

    started SOAP

---
+++ src/main/java/com/qa/interoperability/soap/SOAPActivator.java
@@ -0,0 +1,10 @@
+package com.qa.interoperability.soap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("soap")
+public class SOAPActivator extends Application{
+
+}
